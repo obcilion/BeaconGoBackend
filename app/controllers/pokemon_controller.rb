82f2de92ca84b5df5f73661efd4c98de4474e152
obcilion <raymond.gulbrandsen@shortcut.no>
2017-03-15 20:05:27 +0100

    Yeah

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,6 +1,8 @@
 class PokemonController < ApplicationController
   def get_latest
-    latest_pokemon = Pokemon.where('updated_at > ?', Time.at params[:last_update])
+    latest_pokemon = Pokemon.where(
+      'updated_at > ?',
+      Time.at(params[:last_update]))
     render json: latest_pokemon
   end
 end
